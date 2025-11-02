f0e38eb465e56f4821200b6b27d523264039f9d5
Yehor Petrov <yehorpetrov5@gmail.com>
2025-11-02 12:35:15 +0000

    feat: ability to sort tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,9 +18,29 @@ routerTask.post('/tasks', auth, async (req, res) => {
 });
 
 routerTask.get('/tasks', auth, async (req, res) => {
+  const match = { };
+  const sort = { };
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+  }
+
   try {
     // const tasks = await Task.find({ owner: req.user._id }); // #1
-    await req.user.populate('tasks'); // #2
+    await req.user.populate({
+      path: 'tasks',
+      match: match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort: sort
+      }
+    }); // #2
     res.status(200).send(req.user.tasks);
   } catch (e) {
     res.status(500).send(e);
